426eb050d1fabb5e406ba81bdf42c4f8ceb40211
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-07 15:27:51 -0600

    through top content

--- src/index.js
+++ src/index.js
@@ -40,3 +40,42 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+//images
+
+ const logoImg = document.querySelector('#logo-img');
+ logoImg.src = siteContent.images['logo-img'];
+ 
+ const ctaImg = document.querySelector('#cta-img');
+ ctaImg.src = siteContent.images['cta-img'];
+ 
+ const accentImg = document.querySelector('#middle-img')
+ accentImg.src = siteContent.images['accent-img'];
+
+ //footer
+
+ const footerLink = document.querySelector('footer a');
+ footerLink.textContent = siteContent.footer.copyright;
+ footerLink.classList.add('bold')
+
+ //contact
+
+ const contact = document.querySelector('section.contact');
+ contact.querySelector('h4').textContent = siteContent.contact['contact-h4'];
+ contact.children[1].textContent = siteContent.contact['address'];
+ contact.children[2].textContent = siteContent.contact['phone'];
+ contact.children[3].textContent = siteContent.contact['email'];
+
+ //main-content
+
+ //TOP
+
+ const topContent = document.querySelector('.top-content');
+ topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+ topContent.children[0].children[1].textContent = siteContent['main-content']['features-content'];
+ topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
+ topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
+
+
+
+
+
