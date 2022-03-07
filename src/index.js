918a2e51d4195254f38751be8d5979c8f05e2a41
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-07 16:06:57 -0600

    finished project

--- src/index.js
+++ src/index.js
@@ -75,7 +75,27 @@ console.log('project wired!')
  topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
  topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
 
+//BOTTOM 
+
+  const bottomContent = document.querySelector('.bottom-content');
+  bottomContent.children[0].children[0].textContent = siteContent['main-content']['services-h4'];
+  bottomContent.children[0].children[1].textContent = siteContent['main-content']['services-content'];
+  bottomContent.children[1].children[0].textContent = siteContent['main-content']['product-h4'];
+  bottomContent.children[1].children[1].textContent = siteContent['main-content']['product-content'];
+  bottomContent.children[2].children[0].textContent = siteContent['main-content']['vision-h4'];
+  bottomContent.children[2].children[1].textContent = siteContent['main-content']['vision-content'];
 
 
+//CTA
 
+document.querySelector('.cta h1').textContent = siteContent.cta.h1;
+document.querySelector('.cta button').textContent = siteContent.cta.button;
 
+//NAV
+
+const navContent = document.querySelectorAll('header nav a');
+const navContentText = Object.values(siteContent.nav);
+navContent.forEach((link, index) => {
+  link.textContent = navContentText[index]
+  link.classList.add('italic');
+})
